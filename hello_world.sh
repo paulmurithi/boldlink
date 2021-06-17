c3e0ce1960e76ca749ab41fc4fc024b3ab9efdd5
paulmurithi <murithipaul1@gmail.com>
2021-06-17 13:37:13 +0300

    initial commit

---
+++ hello_world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# print hello world
+echo Hello World
+# request the user to enter the name
+echo Please enter your name:
+# store the value of the name entered in a variable
+read Name
+# great the user
+echo Hello, $Name
---
+++ readme.md
@@ -0,0 +1,4 @@
+1. Unzip the folder
+2. change directory to **hello_wold** using `cd hello_world`
+3. make the **hello_world.sh** file **executable** using `chmod +x hello_world.sh`
+4. Run the script using `./hello_world.sh`
